cd8e63f38997e906c4eba8e53b99b5a116c256c6
Marek Pacześny <mpaczes@tlen.pl>
2025-06-24 15:27:51 +0200

    uzcyie uslugi CatalogService

--- src/main/java/controller/TestPolaczenia.java
+++ src/main/java/controller/TestPolaczenia.java
@@ -5,23 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import mapper.CategoryMapper;
+import service.CatalogService;
 
 @Controller
 public class TestPolaczenia {
-	
-	private CategoryMapper categoryMapper;
-	
-	public TestPolaczenia(CategoryMapper categoryMapper) {
-		this.categoryMapper = categoryMapper;
+
+	private CatalogService catalogService;
+
+	public TestPolaczenia(CatalogService catalogService) {
+		this.catalogService = catalogService;
 	}
 
 	// http://localhost:8080/spring_web_mvc/app/testPolaczenia
 	@RequestMapping(value = "/testPolaczenia", method = { RequestMethod.GET })
 	public String test(Model model) {
 		model.addAttribute("message", "Test polaczenia z baza danych udany!");
-		model.addAttribute("categoryList", categoryMapper.getCategoryList());
+		model.addAttribute("categoryList", catalogService.getCategoryList());
 		return "testPolaczenia";
 	}
-	
+
 }
