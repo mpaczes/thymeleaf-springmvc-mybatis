0af0f85cd321fccfac123fbac3add34e90c8bff1
Marek Pacześny <mpaczes@tlen.pl>
2025-06-24 14:39:51 +0200

    dodanie konsoli H2 i obslugi mapera z kategoriami

--- src/main/java/controller/TestPolaczenia.java
+++ src/main/java/controller/TestPolaczenia.java
@@ -5,13 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import mapper.CategoryMapper;
+
 @Controller
 public class TestPolaczenia {
+	
+	private CategoryMapper categoryMapper;
+	
+	public TestPolaczenia(CategoryMapper categoryMapper) {
+		this.categoryMapper = categoryMapper;
+	}
 
 	// http://localhost:8080/spring_web_mvc/app/testPolaczenia
 	@RequestMapping(value = "/testPolaczenia", method = { RequestMethod.GET })
 	public String test(Model model) {
 		model.addAttribute("message", "Test polaczenia z baza danych udany!");
+		model.addAttribute("categoryList", categoryMapper.getCategoryList());
 		return "testPolaczenia";
 	}
 	
